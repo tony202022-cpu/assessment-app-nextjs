95836afb916fc0210e64061abf81dbbe0ec2c485
tony202022-cpu <tony202022@gmail.com>
2026-01-10 13:55:55 +1100

    Fix profile page dynamic rendering

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Header from "@/components/header";
