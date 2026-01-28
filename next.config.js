32303ae975264822878b154c0e3332ce8260aa5e
[dyad] <git@dyad.sh>
2026-01-29 04:31:57 +1100

    [dyad] wrote 1 file(s)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('chrome-aws-lambda');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
